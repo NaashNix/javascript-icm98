8e53d25d819a99cfa7765db248638952ac26ca7d
NaashNix <thilakshanaravindu@gmail.com>
2023-07-06 20:42:16 +0530

    Printing name on webpage

--- script/main.js
+++ script/main.js
@@ -1,5 +1,7 @@
 const customerName = document.getElementById("customerName");
+const customerNamePrinted = document.getElementById("customerNamePrinted");
 
 function submitClicked(){
    console.log(customerName.value);
+   customerNamePrinted.innerText = customerName.value;
 }
