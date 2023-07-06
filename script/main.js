d6f641b29146e3e2ea8c117a691540bd92e80ccd
NaashNix <thilakshanaravindu@gmail.com>
2023-07-06 20:49:55 +0530

    All properties printed on the web

--- script/main.js
+++ script/main.js
@@ -1,7 +1,25 @@
+// Input Fields
 const customerName = document.getElementById("customerName");
+const customerAddress = document.getElementById("customerAddress");
+const customerSalary = document.getElementById("customerSalary");
+
+// h3 element
 const customerNamePrinted = document.getElementById("customerNamePrinted");
+const customerAddressPrinted = document.getElementById("customerAddressPrinted");
+const customerSalaryPrinted = document.getElementById("customerSalaryPrinted");
+
 
 function submitClicked(){
-   console.log(customerName.value);
-   customerNamePrinted.innerText = customerName.value;
+   console.log(customerName.value,customerAddress.value,customerSalary.value);
+
+   // Setting on web page
+   let name = customerName.value;
+   let address = customerAddress.value;
+   let salary = customerSalary.value;
+
+   customerNamePrinted.innerText = name;
+   customerAddressPrinted.innerText = address;
+   customerSalary.innerText = salary;
+
+
 }
